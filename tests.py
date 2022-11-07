9d115123fa2d4bdb69ea50108df81c9ac44d3eff
yizhijuan <juan111zy@gmail.com>
2022-11-06 23:56:42 -0800

    new

--- tests.py
+++ tests.py
@@ -2,4 +2,13 @@ import unittest
 import logic
 
 class TestLogic(unittest.TestCase):
-    
+    def test_get_winner(self):
+        board = [
+            ['X', None, '0'],
+            [None, 'X', None],
+            [None,'O', 'x'],
+        ]
+        self.assertEqual(logic.get_winner (board), 'X')
+    # TODO: Test all functions from logic.py!
+if __name__ == '__main__'
+    unittest.main()
